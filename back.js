b276f641cc5612840aa0678813c7d53a6ac1d31d
Biel Moliner <microb302@gmail.com>
2022-03-22 21:30:35 +0100

    first commit

---
+++ back.js
@@ -0,0 +1,12 @@
+
+let dia = "Miercoles";
+dia = "Martes";
+let ques = "Hoy es ";
+
+
+console.log(dia);
+console.log("Hola, soy un estudiante Hackademy.");
+
+document.write("¡Empezamos con javascript! ");
+document.write(ques + dia);
+
